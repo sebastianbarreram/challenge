9c720a9cfcfe96c549fd11bbfaa2086f5553750d
Sebastian Barrera Marin <sbarrera.96@hotmail.com>
2024-07-19 02:22:25 -0500

    feat: webpack config

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,12 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swagger-configuration';
 import serviceConfiguration from './config/service-configuration';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs/api', app, document);
-  const cspDefaults = helmet.contentSecurityPolicy.getDefaultDirectives();
-  delete cspDefaults['upgrade-insecure-requests'];
 
-  app.use(
-    helmet({
-      contentSecurityPolicy: { directives: cspDefaults },
-    }),
-  );
   await app.listen(serviceConfiguration().service.port || 3000);
 }
 bootstrap();
